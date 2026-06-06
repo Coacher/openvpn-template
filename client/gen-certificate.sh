8873b24c2a10b6f3f2beb712ed6a138a362ed1a8
Ilya Tumaykin <itumaykin@gmail.com>
2026-06-06 16:01:53 +0300

    cert: add required extensions

--- client/gen-certificate.sh
+++ client/gen-certificate.sh
@@ -1,1 +1,9 @@
-../server/gen-certificate.sh
+#!/usr/bin/bash
+openssl req \
+    -x509 -newkey ed25519 -noenc -days 3650 \
+    -keyout private.key -out public.crt \
+    -subj "/CN=Server" \
+    -addext "basicConstraints=CA:FALSE" \
+    -addext "keyUsage=critical,digitalSignature" \
+    -addext "extendedKeyUsage=clientAuth" \
+    > /dev/null 2>&1
--- server/gen-certificate.sh
+++ server/gen-certificate.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 openssl req \
-    -x509 -newkey ed25519 -noenc -days 3650 -subj "/CN=common-name" \
-    -keyout private.key -out public.crt > /dev/null 2>&1
+    -x509 -newkey ed25519 -noenc -days 3650 \
+    -keyout private.key -out public.crt \
+    -subj "/CN=Server" \
+    -addext "basicConstraints=CA:FALSE" \
+    -addext "keyUsage=critical,digitalSignature" \
+    -addext "extendedKeyUsage=serverAuth" \
+    > /dev/null 2>&1
