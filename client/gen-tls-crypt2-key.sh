6804a48f21aad45fe61a1e85beda3135a19683d5
Ilya Tumaykin <itumaykin@gmail.com>
2026-06-06 18:15:22 +0300

    Drop obsolete files
    TLS Crypt V2 is not supported on clients for now.

--- client/gen-tls-crypt2-key.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-openvpn --tls-crypt-v2 "../server/tls-crypt2-server-$(hostname --short).key" \
-    --genkey tls-crypt-v2-client "tls-crypt2-client-$(hostname --short).key"
--- server/gen-tls-crypt2-key.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-openvpn --genkey tls-crypt-v2-server "tls-crypt2-server-$(hostname --short).key"
